c4810693ce7b35f757d589881ce7dd95c268ed93
codingwithaditya <zinjurkeaditya2@gmail.com>
2025-03-21 19:42:06 +0530

    Added New Files

--- 30 Days Coding/src/com/aditya/Day_1/Main.java
+++ 30 Days Coding/src/com/aditya/Day_1/Main.java
@@ -1,4 +1,25 @@
 package com.aditya.Day_1;
 
+
+//Kth Largest Element
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,4,3,8,4,9,2,5,6};
+        int ans = kthLargest(arr);
+        System.out.println(ans);
+
+    }
+    public static int kthLargest(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
 }
---
+++ 30 Days Coding/src/com/aditya/Day_2/Main.java
@@ -0,0 +1,21 @@
+package com.aditya.Day_2;
+//Given a sorted array arr[] and a number x,
+//write a function to count the occurrences of x in arr[].
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10,14,19,26,27,14,33,35,42,14};
+        int x = 14;
+        int ans = occOfX(arr,x);
+        System.out.println(ans);
+
+    }
+    public static int occOfX(int[] arr,int x) {
+        int rep = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                rep++;
+            }
+        }
+        return rep;
+    }
+}
